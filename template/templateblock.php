cd8dad5322031051421d01e23273d77c9d18463d
Ivan Jan Malong <ije.malong@gmail.com>
2022-04-19 16:09:11 +0800

    Custom Block Settings/Display

--- template/templateblock.php
+++ template/templateblock.php
@@ -27,44 +27,44 @@ $clientsecret		= get_field('client_secret');
 
 ?>
 
-<div class="acf-block-fields acf-fields">
-    <div class="acf-field acf-field-url acf-field-625e58459c5f5 is-required" data-name="wordpress_site" data-type="url" data-key="field_625e58459c5f5" data-required="1">
+<div>
+    <div>
         <div class="acf-label">
-            <label for="acf-block_625e5c56e3cf8-field_625e58459c5f5">Wordpress Site <span class="acf-required">*</span>
+            <label>Wordpress Site <span class="acf-required">*</span>
             </label>
         </div>
         <div class="acf-input">
-            <div class="acf-input-wrap acf-url -valid">
+            <div class="acf-input-wrap acf-url">
                 <i class="acf-icon -globe -small"></i>
-                <input type="url" id="acf-block_625e5c56e3cf8-field_625e58459c5f5" name="acf-block_625e5c56e3cf8[field_625e58459c5f5]" value="<?php echo esc_attr($url); ?>" required="required">
+                <input type="url" id="acf-block_625e5c56e3cf8-field_625e58459c5f5" name="acf-block_625e5c56e3cf8[field_625e58459c5f5]" value="dsfdsfds" required="required">
             </div>
         </div>
     </div>
         
-    <div class="acf-field acf-field-group acf-field-625e58579c5f6" data-name="oauth2_api_credentials" data-type="group" data-key="field_625e58579c5f6">
-        <div class="acf-label">
-            <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6">OAuth2 API credentials</label>
+    <div>
+        <div>
+            <label>OAuth2 API credentials</label>
         </div>
 
-        <div class="acf-input">
-            <div class="acf-fields -top -border">
-                <div class="acf-field acf-field-email acf-field-625e58839c5f7 is-required" data-name="client_id" data-type="email" data-key="field_625e58839c5f7" data-required="1">
-                    <div class="acf-label">
-                        <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e58839c5f7">Client ID <span class="acf-required">*</span></label>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <label>Client ID <span class="acf-required">*</span></label>
                     </div>
-                    <div class="acf-input">
-                    <div class="acf-input-wrap">
-                        <input type="email" id="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e58839c5f7" name="acf-block_625e5c56e3cf8[field_625e58579c5f6][field_625e58839c5f7]" value="<?php echo esc_attr($clientid); ?>" required="required">
+                    <div>
+                        <div>
+                            <input type="text" value="<?php echo esc_attr($clientid); ?>" required="required">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="acf-field acf-field-password acf-field-625e589d9c5f8 is-required" data-name="client_secret" data-type="password" data-key="field_625e589d9c5f8" data-required="1">
-                <div class="acf-label">
-                    <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e589d9c5f8">Client Secret <span class="acf-required">*</span></label>
+            <div>
+                <div>
+                    <label >Client Secret <span class="acf-required">*</span></label>
                 </div>
-            <div class="acf-input">
-                <div class="acf-input-wrap">
-                    <input type="password" id="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e589d9c5f8" name="acf-block_625e5c56e3cf8[field_625e58579c5f6][field_625e589d9c5f8]" value="<?php echo esc_attr($clientsecret); ?>" required="required">
+            <div>
+                <div>
+                    <input type="text" value="<?php echo esc_attr($clientsecret); ?>" required="required">
                 </div>
             </div>
         </div>
