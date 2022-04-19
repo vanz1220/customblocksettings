43b1074d5ee84486dfd50121dfc4c4e7902ff13c
Ivan Jan Malong <ije.malong@gmail.com>
2022-04-19 15:47:43 +0800

    Custom Block Settings/Display

--- template/templateblock.php
+++ template/templateblock.php
@@ -27,8 +27,46 @@ $clientsecret		= get_field('client_secret');
 
 ?>
 
-<div class="<?php echo esc_attr($className); ?>" id="<?php echo esc_attr($id); ?>">
-    <input type="text" value="<?php echo $url; ?>"></br>
-    <input type="text" value="<?php echo $clientid; ?>"></br>
-    <input type="text" value="<?php echo $clientsecret; ?>">
+<div class="acf-block-fields acf-fields">
+    <div class="acf-field acf-field-url acf-field-625e58459c5f5 is-required" data-name="wordpress_site" data-type="url" data-key="field_625e58459c5f5" data-required="1">
+        <div class="acf-label">
+            <label for="acf-block_625e5c56e3cf8-field_625e58459c5f5">Wordpress Site <span class="acf-required">*</span>
+            </label>
+        </div>
+        <div class="acf-input">
+            <div class="acf-input-wrap acf-url -valid">
+                <i class="acf-icon -globe -small"></i>
+                <input type="url" id="acf-block_625e5c56e3cf8-field_625e58459c5f5" name="acf-block_625e5c56e3cf8[field_625e58459c5f5]" value="<?php echo esc_attr($url); ?>" required="required">
+            </div>
+        </div>
+    </div>
+        
+    <div class="acf-field acf-field-group acf-field-625e58579c5f6" data-name="oauth2_api_credentials" data-type="group" data-key="field_625e58579c5f6">
+        <div class="acf-label">
+            <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6">OAuth2 API credentials</label>
+        </div>
+
+        <div class="acf-input">
+            <div class="acf-fields -top -border">
+                <div class="acf-field acf-field-email acf-field-625e58839c5f7 is-required" data-name="client_id" data-type="email" data-key="field_625e58839c5f7" data-required="1">
+                    <div class="acf-label">
+                        <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e58839c5f7">Client ID <span class="acf-required">*</span></label>
+                    </div>
+                    <div class="acf-input">
+                    <div class="acf-input-wrap">
+                        <input type="email" id="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e58839c5f7" name="acf-block_625e5c56e3cf8[field_625e58579c5f6][field_625e58839c5f7]" value="<?php echo esc_attr($clientid); ?>" required="required">
+                    </div>
+                </div>
+            </div>
+            <div class="acf-field acf-field-password acf-field-625e589d9c5f8 is-required" data-name="client_secret" data-type="password" data-key="field_625e589d9c5f8" data-required="1">
+                <div class="acf-label">
+                    <label for="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e589d9c5f8">Client Secret <span class="acf-required">*</span></label>
+                </div>
+            <div class="acf-input">
+                <div class="acf-input-wrap">
+                    <input type="password" id="acf-block_625e5c56e3cf8-field_625e58579c5f6-field_625e589d9c5f8" name="acf-block_625e5c56e3cf8[field_625e58579c5f6][field_625e589d9c5f8]" value="<?php echo esc_attr($clientsecret); ?>" required="required">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
