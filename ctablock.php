577bf54e1ae91bc12ee64e698a211122d4e35101
Ivan Jan Malong <ije.malong@gmail.com>
2022-04-19 12:43:35 +0800

    custom block settings plugin

--- ctablock.php
+++ ctablock.php
@@ -23,3 +23,78 @@
         ));
     }
  }
+
+ function custom_colors(){
+
+    add_theme_support(
+        'editor-color-palette', 
+            array(
+                array(
+                    'name'  => __( 'White', 'textdomain' ),
+                    'slug'  => 'white',
+                    'color'	=> '#FFFFFF',
+                ),
+              array(
+                    'name'	=> __( 'Black', 'textdomain' ),
+                    'slug'	=> 'black',
+                    'color'	=> '#000000',
+                ),
+                array(
+                    'name'  => __( 'Yellow', 'textdomain' ),
+                    'slug'  => 'yellow',
+                    'color'	=> '#ffd000',
+                ),
+                array(
+                    'name'  => __( 'Blue', 'textdomain' ),
+                    'slug'  => 'blue',
+                    'color' => '#347ab7',
+                ),
+                array(
+                    'name'  => __( 'Green', 'textdomain' ),
+                    'slug'  => 'green',
+                    'color' => '#2e924d',
+                ),
+                array(
+                    'name'	=> __( 'Gray', 'textdomain' ),
+                    'slug'	=> 'gray',
+                    'color'	=> '#464646',
+                ),
+                array(
+                    'name'	=> __( 'Light Gray', 'textdomain' ),
+                    'slug'	=> 'lightgray',
+                    'color'	=> '#ebebeb',
+                )
+            )
+    );
+    add_theme_support(
+        'editor-font-sizes', 
+            array(
+                array(
+                    'name' => 'Small',
+                    'slug' => 'small',
+                    'size' => 10
+                    
+                ),
+                array(
+                    'name' => 'Normal',
+                    'slug' => 'normal',
+                    'size' => 15
+                    
+                ),
+                array(
+                    'name' => 'Large',
+                    'slug' => 'large',
+                    'size' => 30
+                    
+                ),
+                array(
+                    'name' => 'X-Large',
+                    'slug' => 'xlarge',
+                    'size' => 50
+                    
+                )
+            )
+    );
+}
+
+add_action('init', 'custom_colors');
